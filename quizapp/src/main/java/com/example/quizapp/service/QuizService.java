871a02a67e5dd96f29df2d0d5f94015451721a8c
Shashi <surveshashikant2004@Gmail.com>
2025-12-22 21:55:34 +0530

    First commit

---
+++ quizapp/src/main/java/com/example/quizapp/service/QuizService.java
@@ -0,0 +1,52 @@
+package com.example.quizapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.example.quizapp.dao.QuestionDao;
+import com.example.quizapp.dao.QuizDao;
+import com.example.quizapp.model.Question;
+import com.example.quizapp.model.QuestionWrapper;
+import com.example.quizapp.model.Quiz;
+
+@Service
+public class QuizService {
+
+	@Autowired
+	private QuizDao quizDao;
+
+	@Autowired
+	private QuestionDao questionDao;
+
+	public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+
+		List<Question> questions = questionDao.findRandomQuestionsByCategory(category, numQ);
+		Quiz quiz = new Quiz();
+		quiz.setTitle(title);
+		quiz.setQuestions(questions);
+		quizDao.save(quiz);
+		return new ResponseEntity<>("Success", HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+
+		Optional<Quiz> quiz = quizDao.findById(id);
+		List<Question> questionsFromDB = quiz.get().getQuestions();
+		List<QuestionWrapper> questionsForUser = new ArrayList<QuestionWrapper>();
+
+		for (Question q : questionsFromDB) {
+			QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(),
+					q.getOption3(), q.getOption4());
+			questionsForUser.add(qw);
+		}
+
+		return new ResponseEntity<List<QuestionWrapper>>(questionsForUser, HttpStatus.OK);
+	}
+
+}
